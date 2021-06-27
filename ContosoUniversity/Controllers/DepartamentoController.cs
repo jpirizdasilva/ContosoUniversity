d08fac64014ebcd7146cf16f05f048abf30769e5
Jeremias Piriz da Silva <jeremias.piriz.da.silva@gmail.com>
2021-06-27 12:49:54 -0300

    Se actualizó los métodos de edición de DepartamentoController

--- ContosoUniversity/Controllers/DepartamentoController.cs
+++ ContosoUniversity/Controllers/DepartamentoController.cs
@@ -36,6 +36,7 @@ namespace ContosoUniversity.Controllers
 
             var departamento = await _context.Departamentos
                 .Include(d => d.Administrador)
+                .AsNoTracking()
                 .FirstOrDefaultAsync(m => m.DepartamentoID == id);
             if (departamento == null)
             {
@@ -77,7 +78,11 @@ namespace ContosoUniversity.Controllers
                 return NotFound();
             }
 
-            var departamento = await _context.Departamentos.FindAsync(id);
+            var departamento = await _context.Departamentos
+                .Include(i => i.Administrador)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.DepartamentoID == id);
+
             if (departamento == null)
             {
                 return NotFound();
@@ -91,35 +96,82 @@ namespace ContosoUniversity.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("DepartamentoID,Nombre,Presupuesto,FechaInicio,InstructorID,RowVersion")] Departamento departamento)
+        public async Task<IActionResult> Edit(int? id, byte[] rowVersion)
         {
-            if (id != departamento.DepartamentoID)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
+
+            var departamentoAActualizar = await _context.Departamentos.Include(i => i.Administrador).FirstOrDefaultAsync(m => m.DepartamentoID == id);
+
+            if (departamentoAActualizar == null)
+            {
+                Departamento DepartamentoEliminado = new Departamento();
+                await TryUpdateModelAsync(DepartamentoEliminado);
+                ModelState.AddModelError(string.Empty,
+                    "No se pueden guardar los cambios. El departamento fue eliminado por otro usuario.");
+                ViewData["InstructorID"] = new SelectList(_context.Instructores, "ID", "NombreCompleto", DepartamentoEliminado.InstructorID);
+                return View(DepartamentoEliminado);
+            }
+
+            _context.Entry(departamentoAActualizar).Property("RowVersion").OriginalValue = rowVersion;
+
+            if (await TryUpdateModelAsync<Departamento>(
+                departamentoAActualizar,
+                "",
+                s => s.Nombre, s => s.FechaInicio, s => s.Presupuesto, s => s.InstructorID))
             {
                 try
                 {
-                    _context.Update(departamento);
                     await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
                 }
-                catch (DbUpdateConcurrencyException)
+                catch (DbUpdateConcurrencyException ex)
                 {
-                    if (!DepartamentoExists(departamento.DepartamentoID))
+                    var exceptionEntry = ex.Entries.Single();
+                    var clientValues = (Departamento)exceptionEntry.Entity;
+                    var databaseEntry = exceptionEntry.GetDatabaseValues();
+                    if (databaseEntry == null)
                     {
-                        return NotFound();
+                        ModelState.AddModelError(string.Empty,
+                            "Imposible guardar los cambios. El departamento fue eliminado por otro usuario.");
                     }
                     else
                     {
-                        throw;
+                        var databaseValues = (Departamento)databaseEntry.ToObject();
+
+                        if (databaseValues.Nombre != clientValues.Nombre)
+                        {
+                            ModelState.AddModelError("Nombre", $"Current value: {databaseValues.Nombre}");
+                        }
+                        if (databaseValues.Presupuesto != clientValues.Presupuesto)
+                        {
+                            ModelState.AddModelError("Presupuesto", $"Current value: {databaseValues.Presupuesto:c}");
+                        }
+                        if (databaseValues.FechaInicio != clientValues.FechaInicio)
+                        {
+                            ModelState.AddModelError("FechaInicio", $"Current value: {databaseValues.FechaInicio:d}");
+                        }
+                        if (databaseValues.InstructorID != clientValues.InstructorID)
+                        {
+                            Instructor databaseInstructor = await _context.Instructores.FirstOrDefaultAsync(i => i.ID == databaseValues.InstructorID);
+                            ModelState.AddModelError("InstructorID", $"Current value: {databaseInstructor?.NombreCompleto}");
+                        }
+
+                        ModelState.AddModelError(string.Empty, "The record you attempted to edit "
+                                + "was modified by another user after you got the original value. The "
+                                + "edit operation was canceled and the current values in the database "
+                                + "have been displayed. If you still want to edit this record, click "
+                                + "the Save button again. Otherwise click the Back to List hyperlink.");
+                        departamentoAActualizar.RowVersion = (byte[])databaseValues.RowVersion;
+                        ModelState.Remove("RowVersion");
                     }
                 }
-                return RedirectToAction(nameof(Index));
             }
-            ViewData["InstructorID"] = new SelectList(_context.Instructores, "ID", "NombreCompleto", departamento.InstructorID);
-            return View(departamento);
+            ViewData["InstructorID"] = new SelectList(_context.Instructores, "ID", "FullName", departamentoAActualizar.InstructorID);
+            return View(departamentoAActualizar);
         }
 
         // GET: Departamento/Delete/5
